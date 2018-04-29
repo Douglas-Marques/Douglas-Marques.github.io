e37f414050ca4d77e4c4548c2d756d429bb67622
Douglas Marques <douglasmarques09@gmail.com>
2018-04-29 18:15:51 -0300

    Add files via upload

--- GrauCCalculator/app.js
+++ GrauCCalculator/app.js
@@ -20,42 +20,46 @@ function calculateResults() {
   const grauAFinal = document.getElementById('grauAFinal');
   const grauBFinal = document.getElementById('grauBFinal');
 
-  //Compute notaFinal
-  const novoGrauA = parseFloat(grauA.value) * 0.33;
-  let novoGrauB = 0.0;
-  let conditional = false;
-  let notaFinal = 0.0;
-  //Compute grauB if is blank
-  if(grauB.value === '') {
-    conditional = true;
-    novoGrauB = (6 - novoGrauA) * 1.493;
-    showMessage('Você precisa tirar ' + novoGrauB.toFixed(2) + ' para ficar na média', 'alert-info');
-    notaFinal = novoGrauA + novoGrauB;
+  if(grauA.value === '' && grauB.value === '') {
+    showMessage('Os campos estão vazios!', 'alert-danger' )
   } else {
-    novoGrauB = parseFloat(grauB.value) * 0.67;
-    notaFinal = novoGrauA + novoGrauB;
-  }
+    //Compute notaFinal
+    const novoGrauA = parseFloat(grauA.value) * 0.33;
+    let novoGrauB = 0.0;
+    let conditional = false;
+    let notaFinal = 0.0;
+    //Compute grauB if is blank
+    if(grauB.value === '') {
+      conditional = true;
+      novoGrauB = (6 - novoGrauA) * 1.493;
+      showMessage('Você precisa tirar ' + novoGrauB.toFixed(2) + ' para ficar na média', 'alert-info');
+      notaFinal = novoGrauA + novoGrauB;
+    } else {
+      novoGrauB = parseFloat(grauB.value) * 0.67;
+      notaFinal = novoGrauA + novoGrauB;
+    }
 
-  if(isFinite(notaFinal) && notaFinal <= 10) {
-    //Clear inputs
-    grauA.value = '';
-    grauB.value = '';
+    if(isFinite(notaFinal) && notaFinal <= 10) {
+      //Clear inputs
+      grauA.value = '';
+      grauB.value = '';
 
-    //Show points
-    grauAFinal.value = novoGrauA.toFixed(2);
-    grauBFinal.value = conditional === false ? novoGrauA.toFixed(2) : (novoGrauB * 0.67).toFixed(2);
-    grauFinal.value = notaFinal.toFixed(2);
+      //Show points
+      grauAFinal.value = novoGrauA.toFixed(2);
+      grauBFinal.value = conditional === false ? novoGrauA.toFixed(2) : (novoGrauB * 0.67).toFixed(2);
+      grauFinal.value = notaFinal.toFixed(2);
 
-    //Show Results
-    document.getElementById('results').style.display = 'block';
+      //Show Results
+      document.getElementById('results').style.display = 'block';
 
-    //Hide loader
-    document.getElementById('loading').style.display = 'none';
+      //Hide loader
+      document.getElementById('loading').style.display = 'none';
 
-    //Show Message
-    conditional === false ? grauFinal.value >= 6 ? showMessage('Parabéns! Você foi aprovado!', 'alert-success') : showMessage('Parece que alguém vai ter que fazer o GrauC...', 'alert-warning') : '';
-  } else {
-    showMessage('Algo deu errado! Confira se as notas estão corretas.', 'alert-danger');
+      //Show Message
+      conditional === false ? grauFinal.value >= 6 ? showMessage('Parabéns! Você foi aprovado!', 'alert-success') : showMessage('Parece que alguém vai ter que fazer o GrauC...', 'alert-warning') : '';
+    } else {
+      showMessage('Algo deu errado! Confira se as notas estão corretas.', 'alert-danger');
+    }
   }
 }
 
