2de14d3704b178e567cf6f76120c0af7d41f3335
Douglas Marques <douglasmarques09@gmail.com>
2018-05-14 20:48:36 -0300

    Update ui.js

--- climaoJS/ui.js
+++ climaoJS/ui.js
@@ -15,7 +15,7 @@ class UI {
     this.location.textContent = `${weather.name}, ${weather.state}`;
     this.desc.textContent = weather.data.temperature + ' °C';
     this.string.textContent = weather.data.condition;
-    this.humidity.textContent = `Umidade relativa do ar: ${weather.data.humidity}`;
+    this.humidity.textContent = `Umidade relativa do ar: ${weather.data.humidity} %`;
     this.feelsLike.textContent = `Atualização: ${weather.data.date}`;
     this.dewpoint.textContent = `Sensação térmica: ${weather.data.sensation} °C`;
     this.wind.textContent = `Velocidade do vento: ${weather.data.wind_velocity} Km/h`;
